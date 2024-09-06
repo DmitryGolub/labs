d54c8c5de1d79ee38a7751d769dbcf316b634b74
Dmitry Golub <gosest4@gmail.com>
2024-09-06 15:20:22 +0300

    first and second lab

---
+++ 1_lab.py
@@ -0,0 +1,46 @@
+def validator_1(data):
+    try:
+        n = int(data)
+
+        if n < 1:
+            print(" -- Число должно быть больше 0 -- ")
+            return False
+    except Exception as ex:
+        print(" -- Ошибка при вводе данных, повторите попытку -- ")
+        return False
+    return n
+
+
+# первая задача
+number = input('Введите число: ')
+
+while not validator_1(number) or validator_1(number) < 1:
+    number = input('Введите число: ')
+
+number = validator_1(number)
+
+for num in range(1, number + 1):
+    print(num)
+
+
+def validator_2(data):
+    try:
+        n = float(data)
+    except Exception as ex:
+        print(" -- Ошибка при вводе данных, повторите попытку -- ")
+        return False
+    return n
+
+
+# вторая задача
+first_number = input('Введите первое число: ')
+
+while not validator_2(first_number):
+    first_number = input('Введите первое число: ')
+
+second_number = input('Введите второе число: ')
+
+while not validator_2(second_number):
+    second_number = input('Введите второе число: ')
+
+print(first_number if first_number > second_number else second_number)
---
+++ 2_lab.py
@@ -0,0 +1,28 @@
+# задание 1
+def greet(name):
+    s = f'Hello, {name}'
+    print(s)
+
+
+def square(number):
+    return number ** 2
+
+
+def max_to_two(number_first, number_second):
+    return number_first if number_first > number_second else number_second
+
+# задание 2
+def describe_person(name, age=30):
+    s = f'Your name: {name}. Your age: {age}'
+    print(s)
+
+
+def is_prime(number):
+    a = []
+    for i in range(1, number+1):
+        if number % i == 0:
+            a.append(i)
+
+    if len(a) > 2:
+        return False
+    return True
