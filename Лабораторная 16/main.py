36f6d28654b0af0dc852467c25e1a2d9eee48fdf
Dmitry Golub <gosest4@gmail.com>
2025-05-25 14:59:46 +0300

    New lab

--- Лабораторная 16/main.py
+++ Лабораторная 16/main.py
@@ -1,15 +1,19 @@
 import face_recognition
 
+
 img1 = face_recognition.load_image_file("img1.jpg")
 img2 = face_recognition.load_image_file("img2.jpg")
 
-img1_encoding = face_recognition.face_encodings(img1)[0]
-img2_encoding = face_recognition.face_encodings(img2)[0]
+try:
+    img1_encoding = face_recognition.face_encodings(img1)[0]
+    img2_encoding = face_recognition.face_encodings(img2)[0]
 
-results = face_recognition.compare_faces([img1_encoding], img2_encoding)
+    results = face_recognition.compare_faces([img1_encoding], img2_encoding)
 
-if results[0]:
-    print("✅ Лица совпадают!")
+    if results[0]:
+        print("✅ Лица совпадают!")
+    else:
+        print("❌ Лица разные!")
 
-else:
-    print("❌ Лица разные!")
+except IndexError:
+    print("Лицо не распознано")
